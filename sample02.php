0f6712516cb8259c0732025070f0b79c5fe8778b
falcon649 <nadundineshskl@gmail.com>
2015-05-13 04:36:39 +0000

    0513 practice

---
+++ sample02.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>sample02</title>
+</head>
+<body>
+    <?php
+    print('<h2 style="font-size: 110%; font-weight: bold;">画面にHTMLを表示する</h2>');
+    print('<p>PHP can write inside the HTML</p>');
+    print('<p>use <br/> br tag for move to the next line</p>');
+    ?>
+</body>
+</html>
