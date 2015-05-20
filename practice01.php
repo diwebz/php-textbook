edf6ea40f8572427d51ae1bc6c88eb4d617d5d05
falcon649 <nadundineshskl@gmail.com>
2015-05-20 03:46:30 +0000

    0513 practice

---
+++ practice01.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>practice01</title>
+</head>
+<body>
+    <?php
+    $myname = 'nadun';
+    //$123 = 'numbers ダメ(if entering numbers, should use 1st one in alphebet)';
+    
+    //$_myname = 'これは ok';
+    
+    //$new = 'これもダメ';
+    //$MYNAME = 'dinesh';
+    
+    
+    print($myname);
+    print('は、');
+    //print('6');
+    print(strlen($myname));
+    print('文字です。');
+    ?>
+    
+</body>
+</html>
